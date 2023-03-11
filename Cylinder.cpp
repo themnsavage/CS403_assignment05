09fbe5ef85f5c42585aaffb0c7eafe2115656258
themnsavage <mnsavage@crimson.ua.edu>
2023-03-11 16:37:09 -0600

    feat(Cylinder): get area and volume function implemented

--- Cylinder.cpp
+++ Cylinder.cpp
@@ -9,9 +9,13 @@ Cylinder::Cylinder(string& name, double radius, double height)
     this->height = height;
 }
 // get the surface area
-double Cylinder::getArea() const{}           
+double Cylinder::getArea() const{
+    return 2*PI * this->radius*this->height + 2*PI * (this->radius * this->radius);
+}
 // get the volume
-double Cylinder::getVolume() const{}
+double Cylinder::getVolume() const{
+    return PI * (this->radius*this->radius) * this->height;
+}
 // test if the conditions are statisfied
 bool Cylinder::test(const vector<string>& cond) const{}
 // get all the info in a string
