40c68b8786014b07f9fdac47b3383b9089ea0d4b
themnsavage <mnsavage@crimson.ua.edu>
2023-03-11 16:50:20 -0600

    feat(Torus): implement get area and volume functions

--- Torus.cpp
+++ Torus.cpp
@@ -16,9 +16,13 @@ Torus::Torus(string& name, double radius1, double radius2)
     }
 }
 // get the surface area
-double Torus::getArea() const{}           
+double Torus::getArea() const{
+    return (4*PI*this->smallRadius)*(4*PI*this->bigRadius);
+}
 // get the volume
-double Torus::getVolume() const{}
+double Torus::getVolume() const{
+    return (PI*(this->smallRadius*this->smallRadius))*(2*PI*this->bigRadius);
+}
 // test if the conditions are statisfied
 bool Torus::test(const vector<string>& cond) const{}
 // get all the info in a string
