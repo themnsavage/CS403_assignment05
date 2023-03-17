a7bce9c57dda2b24d0a64f76d32fac7d63de04b7
themnsavage <mnsavage@crimson.ua.edu>
2023-03-17 05:34:30 -0500

    fix(reading): fix segmentation in reading class and now working

--- project5/reading.cpp
+++ project5/reading.cpp
@@ -5,7 +5,7 @@ using namespace std;
 vector<Shape*> *readFrom(string fileName){
     ifstream ifs(fileName);
     string line;
-    vector <Shape*> shapes;
+    vector <Shape*> *shapes = new vector<Shape*>(0);
     while(getline(ifs,line)){//grap each line from file
         //create a vector with line seperated by spaces
         string word;
@@ -15,24 +15,21 @@ vector<Shape*> *readFrom(string fileName){
             listOfWords.push_back(word);
         }
         //create shapes
-        Shape *newShape = NULL;
         string name = listOfWords[0];
         string type = listOfWords[1];
         if(type.compare("sphere") == 0){
-            newShape = new Sphere(name, stod(listOfWords[2]));
+            shapes->push_back(new Sphere(name, stod(listOfWords[2])));
         }
         else if(type.compare("cylinder") == 0){
-            newShape = new Cylinder(name, stod(listOfWords[2]), stod(listOfWords[3]));
+            shapes->push_back(new Cylinder(name, stod(listOfWords[2]), stod(listOfWords[3])));
         }
         else if(type.compare("torus") == 0){
-            newShape = new Torus(name,stod(listOfWords[2]),stod(listOfWords[3]));
+            shapes->push_back(new Torus(name, stod(listOfWords[2]), stod(listOfWords[3])));
         }
         else if(type.compare("box") == 0){
-            newShape = new Box(name,stod(listOfWords[2]),stod(listOfWords[3]),stod(listOfWords[4]));
+            shapes->push_back(new Box(name, stod(listOfWords[2]), stod(listOfWords[3]), stod(listOfWords[4])));
         }
-        shapes.push_back(newShape);
     }
-    vector<Shape*> * shapePointer = &shapes;
-    return shapePointer;
+    return shapes;
 }
 
